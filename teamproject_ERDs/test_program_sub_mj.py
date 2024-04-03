494ecfb8dabb35c46f3947f982e0f97a3425a8d3
sweetmjkim <sweet.mjkim@gmail.com>
2024-04-03 08:56:04 +0000

    2024.04.03

--- teamproject_ERDs/test_program_sub_mj.py
+++ teamproject_ERDs/test_program_sub_mj.py
@@ -12,6 +12,16 @@ answer_type = int(input("문제 유형을 입력하세요 (N지 선다형): "))
 quest_type = int(input("문제 수를 입력하세요 (N개 문항) : "))
 print("문제와 선택지를 입력하세요:")
 
+# with conn.cursor() as cursor:
+#     sql = "DELETE FROM QUEST_INFO WHERE QUEST_INFO_ID IS NOT NULL"
+#     cursor.execute(sql)
+#     conn.commit()
+
+# with conn.cursor() as cursor:
+#     sql = "DELETE FROM ANSWER_INFO WHERE ANSWER_INFO_ID IS NOT NULL"
+#     cursor.execute(sql)
+#     conn.commit()
+    
 for x in range(quest_type) :
     QUEST = input(f"문항 {x+1}: ")
     try : 
@@ -23,35 +33,40 @@ for x in range(quest_type) :
             for row in data:
                 last = row  # 각 행 출력
             
-            QUEST_INFO_ID_NUMBER = int(last[0][11:])
+            QUEST_INFO_ID = f"QUEST_INFO_{int(last[0][11:])+1}"
     except :
-        QUEST_INFO_ID_NUMBER = 3
+        QUEST_INFO_ID = "QUEST_INFO_ID_1"
     # mysql로 문제 저장
     with conn.cursor() as cursor:
         # Create
         sql = "INSERT INTO QUEST_INFO (QUEST_INFO_ID,QUEST, QUEST_NUMBER) VALUES (%s, %s, %s)"
-        cursor.execute(sql, (f"QUEST_INFO_{QUEST_INFO_ID_NUMBER+1}", QUEST, x+1))
+        cursor.execute(sql, (QUEST_INFO_ID, QUEST, x+1))
         conn.commit()
-        
-    with conn.cursor() as cursor:
-    # Read
-        sql = "SELECT ANSWER_INFO_ID FROM ANSWER_INFO"
-        cursor.execute(sql)
-        data = cursor.fetchall()
-        for row in data:
-            last = row  # 각 행 출력
-        
-        ANSWER_INFO_ID_NUMBER = int(last[0][12:])
-        
+
         
     # 선택지 입력
     print("선택지: ")
     for y in range(answer_type):
+        try : 
+            with conn.cursor() as cursor:
+            # Read
+                sql = "SELECT ANSWER_INFO_ID FROM ANSWER_INFO"
+                cursor.execute(sql)
+                data = cursor.fetchall()
+                for row in data:
+                    last = row  # 각 행 출력
+                
+                ANSWER_INFO_ID = f"ANSWER_INFO_{int(last[0][12:])+1}" 
+        except : 
+                ANSWER_INFO_ID = "ANSWER_INFO_1"
+            
+        
         answer = input(f"{y+1}. ")
-        # Create
-        sql = "INSERT INTO ANSWER_INFO (ANSWER_INFO_ID,ANSWER, ANSWER_NUMBER, QUEST_INFO_ID) VALUES (%s, %s, %s, %s)"
-        cursor.execute(sql, (f"ANSWER_INFO_{ANSWER_INFO_ID_NUMBER+1}", answer, y+1, f"QUEST_INFO_{x+1}"))
-        conn.commit()
+        with conn.cursor() as cursor:
+            # Create
+            sql = "INSERT INTO ANSWER_INFO (ANSWER_INFO_ID,ANSWER, ANSWER_NUMBER, QUEST_INFO_ID) VALUES (%s, %s, %s, %s)"
+            cursor.execute(sql, (ANSWER_INFO_ID, answer, y+1, QUEST_INFO_ID))
+            conn.commit()
 
 conn.close()
 
